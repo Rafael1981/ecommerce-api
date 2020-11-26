09b62b16bd86b549d2b8b4a131e76ad91f1e9dfd
Rafael Ferreira <r.a.ferreira1981@gmail.com>
2020-11-26 16:28:25 +1100

    First commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::API
+
+  before_action :configure_permited_parameters, if: :devise_controller?
+
+  protected
+  
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:signup, keys: [:name, :email, :password, :password_confirmation])
+  end
+end
